74140999f40d3bc186222ec87a34880799c4197d
Michele del Monaco <micheledelmonaco@hotmail.com>
2018-08-01 18:12:37 +0100

    Removed review section

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+
   devise_for :users
-  resources :homes
+  resources :homes do
+  	  resources :reviews, except: [:show, :index]
+  end
+
   get 'pages/about'
 
   get 'pages/contact'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180801135856) do
+ActiveRecord::Schema.define(version: 20180801163440) do
 
   create_table "homes", force: :cascade do |t|
     t.string "name"
@@ -20,6 +20,15 @@ ActiveRecord::Schema.define(version: 20180801135856) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "image"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer "rating"
+    t.text "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.integer "home_id"
   end
 
   create_table "users", force: :cascade do |t|
